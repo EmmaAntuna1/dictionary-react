dedc89cda6ba71ec6d35ea75299502ca9ae7e8d3
EmmaAntuna1 <88903845+EmmaAntuna1@users.noreply.github.com>
2021-11-07 12:49:33 -0600

    changed background color

--- src/App.js
+++ src/App.js
@@ -10,7 +10,19 @@ export default function App(){
         <main>
           <Dictionary defaultKeyword="yoga" />
         </main>
-        <footer className="App-footer"><small>Coded by Emma Antuna</small></footer>
+        <footer>
+      This project was coded by Emma Antuna and is{" "}
+    <a href="https://github.com/EmmaAntuna1/weather-app-react"
+    target="_blank"
+    rel="noopener noreferrer">
+       open-sourced on github
+    </a>
+    {" "}and 
+    <a href="https://reverent-ritchie-af8e63.netlify.app" target="_blank"
+    rel="noopener noreferrer"> 
+    {" "}hosted on Netlify
+    </a>
+    </footer>
       </div>
     </div>
   );
